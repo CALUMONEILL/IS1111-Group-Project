8b905deda325d4e0f576e97e4e18fc729c80de88
Calum James O Neill <120475986@umail.ucc.ie>
2021-03-07 14:55:07 +0000

    Calum 7/3/21 Pasted some code from last project. Code is for log-in form - different users are taken to different forms corresponding to their job. Commented out all the code for now.

--- Form1.vb
+++ Form1.vb
@@ -6,5 +6,60 @@
         'another test
 
         'TEST
+
+
+
+
+
+
+
+
+
+        'Calum 7/3/21 Some code I used for my login, I had three different users.
+        'Maybe we could use something like this for each worker but each one has a different button to bring them to the right form.
+
+        'eg if the operator logs in they get a button going to the product form. 
+        'If the administrator logs in, the button that goes to the form where they can edit accounts is shown
+
+        'Declare variables
+        'Dim username, password As String
+
+
+        'Assign variable to textbox
+        'username = txtUsername.Text
+        'password = txtPassword.Text
+
+
+        'The application has user data for three phone operators, with one password for all three
+        'Output to Textbox that the inputted details are correct or incorrect
+        'If username = "John" And password = "Password" Then
+        'This makes the button that takes the user to the next page visible
+        '   btnAge.Visible = True
+        '   txtPasswordVer.Text = "Successful Login - Welcome, John"
+
+        'The following code is a repeat of the above but rewritten to allow for two extra operators
+        'ElseIf username = "Paul" And password = "Password" Then
+        '   btnAge.Visible = True
+        '   txtPasswordVer.Text = "Successful Login - Welcome, Paul"
+
+        'ElseIf username = "George" And password = "Password" Then
+        '   btnAge.Visible = True
+        '   txtPasswordVer.Text = "Successful Login - Welcome, George"
+
+        'Else
+        'If the incorrect password or an invalid username is inputted into the respective textboxes, the following message will appear in the bottom-most textbox
+        '   txtPasswordVer.Text = "Invalid Details - Please try again"
+        'This makes the button that takes the user to next page invisible
+        '   btnAge.Visible = False
+        'End If
+
+
+
+
+
+
+
+
+
     End Sub
 End Class
