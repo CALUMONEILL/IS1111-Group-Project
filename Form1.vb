913947367dc0a918a9bc1d0cf79228a75e07c4fe
Calum James O Neill <120475986@umail.ucc.ie>
2021-03-12 09:47:24 +0000

    Calum 12/3/21 Still trying to set up database

--- Form1.vb
+++ Form1.vb
@@ -1,7 +1,6 @@
 ﻿Public Class Form1
     Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
-        'TODO: This line of code loads data into the 'IS1111_GRP_PROJ_AccessDataSet.tblTest' table. You can move, or remove it, as needed.
-        Me.TblTestTableAdapter.Fill(Me.IS1111_GRP_PROJ_AccessDataSet.tblTest)
+
 
 
 
